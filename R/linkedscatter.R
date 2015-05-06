e17f1095d9efb7b9669e55b0693d2f5ea7b0c8b0
Hector Corrada Bravo <hcorrada@gmail.com>
2015-05-06 01:16:02 -0400

    create htmlwidgets scaffold

---
+++ R/linkedscatter.R
@@ -0,0 +1,38 @@
+#' linkedscatter
+#'
+#' Linked scatter plot
+#'
+#' @import htmlwidgets
+#'
+#' @export
+linkedscatter <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'linkedscatter',
+    x,
+    width = width,
+    height = height,
+    package = 'linkedscatter'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+linkedscatterOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'linkedscatter', width, height, package = 'linkedscatter')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderLinkedscatter <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, linkedscatterOutput, env, quoted = TRUE)
+}
