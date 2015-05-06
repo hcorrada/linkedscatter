78c80053e1998b4d9bce412aa7d1168ff6a89ac5
Hector Corrada Bravo <hcorrada@gmail.com>
2015-05-06 02:39:45 -0400

    add tooltip

--- inst/htmlwidgets/linkedscatter.js
+++ inst/htmlwidgets/linkedscatter.js
@@ -8,11 +8,19 @@ HTMLWidgets.widget({
     d3.select(el).append("svg")
         .attr("width", width)
         .attr("height", height);
+
+    var tooltip = d3.select(el).append("div")
+                      .attr("class", "tooltip")
+                      .style("opacity", 0);
+
+    return {
+      tooltip: tooltip,
+    };
   },
 
   renderValue: function(el, x, instance) {
     var data = HTMLWidgets.dataframeToD3(x.data);
-    console.log(data);
+    var tooltip = instance.tooltip;
 
     var selection = d3.select(el).select("svg")
           .selectAll(".dot")
@@ -22,7 +30,24 @@ HTMLWidgets.widget({
         .attr("class", "dot")
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; })
-        .attr("r", 15);
+        .attr("r", 15)
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout);
+
+    function mouseover(d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", 0.9);
+      tooltip.html(d.name)
+        .style("left", (d3.event.pageX + 5) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    }
+
+    function mouseout(d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", 0);
+    }
   },
 
   resize: function(el, width, height, instance) {
