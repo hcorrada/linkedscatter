d32ce748aade694615b2c86b28d1b917e96397a5
Hector Corrada Bravo <hcorrada@gmail.com>
2015-05-06 02:07:57 -0400

    draw circles from data

--- inst/htmlwidgets/linkedscatter.js
+++ inst/htmlwidgets/linkedscatter.js
@@ -8,14 +8,26 @@ HTMLWidgets.widget({
     d3.select(el).append("svg")
         .attr("width", width)
         .attr("height", height);
+
+    var data = [
+      {x: 100, y: 25, name: 'alpha'},
+      {x: 25, y: 28, name: 'beta'},
+      {x: 200, y: 125, name: 'gamma'}
+    ];
+
+    return data;
   },
 
   renderValue: function(el, x, data) {
-    d3.select(el).select("svg")
-      .append("text")
-        .attr("x", 15)
-        .attr("y", 15)
-        .text(x.message);
+    var selection = d3.select(el).select("svg")
+          .selectAll("circle")
+          .data(data);
+
+    selection.enter().append("circle")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", 15)
+        .style("fill", "steelblue");
   },
 
   resize: function(el, width, height, instance) {
