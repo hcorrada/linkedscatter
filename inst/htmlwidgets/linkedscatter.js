28e4f7a6085bac9101701ebfad5c6a23dfe14a1b
Hector Corrada Bravo <hcorrada@gmail.com>
2015-05-06 01:52:57 -0400

    step back, print message as svg text element

--- inst/htmlwidgets/linkedscatter.js
+++ inst/htmlwidgets/linkedscatter.js
@@ -5,26 +5,17 @@ HTMLWidgets.widget({
   type: 'output',
 
   initialize: function(el, width, height) {
-    var svg = d3.select(el)
-                  .append("svg")
-                    .attr("width", width)
-                    .attr("height", height);
-    var data = [
-      {x: 10, y: 20, name: 'alpha'},
-      {x: 50, y: 70, name: 'beta'},
-      {x: 60, y: 90, name: 'gamma'},
-    ];
-
-    return {
-      svg: svg,
-      data: data,
-    };
+    d3.select(el).append("svg")
+        .attr("width", width)
+        .attr("height", height);
   },
 
-  renderValue: function(el, x, instance) {
-
-    el.innerText = x.message;
-
+  renderValue: function(el, x, data) {
+    d3.select(el).select("svg")
+      .append("text")
+        .attr("x", 15)
+        .attr("y", 15)
+        .text(x.message);
   },
 
   resize: function(el, width, height, instance) {
